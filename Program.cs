7c163fecbe28eff8f54ff3905528241864b947f7
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-02-20 15:33:44 +0000

    Implemented user input for rules

--- Program.cs
+++ Program.cs
@@ -1,20 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Call FizzBuzz function as defined in fizzbuzz.cs
-Console.WriteLine("Hello World");
-
+// Ask user for length of FizzBuzz exercise
 Console.WriteLine("Enter your maximum number for FizzBuzz?");
 bool validNum = false;
 int userMaxNumber = 0;
-while(!validNum) {
-    if(int.TryParse(Console.ReadLine(), out int answer)) {
+while (!validNum)
+{
+    if (int.TryParse(Console.ReadLine(), out int answer))
+    {
         userMaxNumber = answer;
         validNum = true;
-    } else { 
+    }
+    else
+    {
         Console.WriteLine("Please enter a valid number");
 
     }
 }
 
-FizzBuzz.FizzBuzzFunc(userMaxNumber);
+// Ask user which rules to implement
+bool[] activatedRules = { false, false, false, false, false, false };
+bool choosingRules = true;
+while (choosingRules)
+{
+    Console.WriteLine("Please enter a rule to add, or exit by typing N:");
+    Console.WriteLine("Rule 1: Fizz  /nl Rule 2: Buzz /nl Rule 3: Bang /nl Rule 4: Bong /nl 5: Fezz  6: Reverse");
+    var response = Console.ReadLine();
+    if (response == "N")
+    {
+        choosingRules = false;
+    }
+    else if (int.TryParse(response, out int answer))
+    {
+        activatedRules[answer - 1] = true;
+    }
+    if (!activatedRules.Contains(false))
+    {
+        choosingRules = false;
+        Console.WriteLine("You've activated all rules!");
+    }
+}
+
+// Call FizzBuzz function as defined in fizzbuzz.cs
+FizzBuzz.FizzBuzzFunc(userMaxNumber, activatedRules);
 
--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -2,20 +2,29 @@
 internal class FizzBuzz
 {
     // Define function that prints fizzbuzz. 
-    public static void FizzBuzzFunc(int maxNumber)
-    {
-        for (int counter = 0; counter <= maxNumber; counter++)
+    public static void FizzBuzzFunc(int maxNumber,
+        bool[] rulesArray)
+    {        
+        // Define rule variable names
+        bool fizzActive = rulesArray[0];
+        bool buzzActive = rulesArray[1];
+        bool bangActive = rulesArray[2];
+        bool bongActive = rulesArray[3];
+        bool fezzActive = rulesArray[4];
+        bool reverseActive = rulesArray[5];
+        
+        for (int counter = 1; counter <= maxNumber; counter++)
         {
             Console.WriteLine(
-              counter % 3 != 0 && counter % 5 != 0 && counter % 7 != 0 && counter % 11 != 0 && counter % 13 != 0
+              (counter % 3 != 0 || !fizzActive) && (counter % 5 != 0 || !buzzActive) && (counter % 7 != 0 || !bangActive) && (counter % 11 != 0 || !bongActive) && (counter % 13 != 0 || !fezzActive)
               ?
               counter
               :
-                counter % 17 != 0
+                (counter % 17 != 0 && !reverseActive) 
                 ?
-                (counter % 3 != 0 ? "" : "fizz") + (counter % 13 != 0 ? "" : "fezz") + (counter % 5 != 0 ? "" : "buzz") + (counter % 7 != 0 ? "" : "bang") + (counter % 11 != 0 ? "" : "bong")
-                : 
-                  ((counter % 11 != 0 ? "" : "bong") + (counter % 7 != 0 ? "" : "bang") + (counter % 5 != 0 ? "" : "buzz") + (counter % 13 != 0 ? "" : "fezz") + (counter % 3 != 0 ? "" : "fizz")));
+                ((counter % 3 != 0 || !fizzActive)? "" : "fizz") + (counter % 13 != 0  || !fezzActive? "" : "fezz") + (counter % 5 != 0 || !buzzActive ? "" : "buzz") + (counter % 7 != 0 || !bangActive ? "" : "bang") + (counter % 11 != 0 || !bongActive ? "" : "bong")
+                :
+                  ((counter % 11 != 0 || !bongActive? "" : "bong") + (counter % 7 != 0 || !bangActive? "" : "bang") + (counter % 5 != 0 || !buzzActive ? "" : "buzz") + (counter % 13 != 0 || !fezzActive? "" : "fezz") + (counter % 3 != 0 || !fizzActive ? "" : "fizz")));
         }
     }
 }
