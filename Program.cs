6804a907e74f7d664251fb82a3f5be2cf3965caa
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-02-20 14:11:06 +0000

    Added user input request

--- Program.cs
+++ Program.cs
@@ -3,5 +3,18 @@
 // Call FizzBuzz function as defined in fizzbuzz.cs
 Console.WriteLine("Hello World");
 
-FizzBuzz.FizzBuzzFunc();
+Console.WriteLine("Enter your maximum number for FizzBuzz?");
+bool validNum = false;
+int userMaxNumber = 0;
+while(!validNum) {
+    if(int.TryParse(Console.ReadLine(), out int answer)) {
+        userMaxNumber = answer;
+        validNum = true;
+    } else { 
+        Console.WriteLine("Please enter a valid number");
+
+    }
+}
+
+FizzBuzz.FizzBuzzFunc(userMaxNumber);
 
--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -2,18 +2,21 @@
 internal class FizzBuzz
 {
     // Define function that prints fizzbuzz. 
-    public static void FizzBuzzFunc()
+    public static void FizzBuzzFunc(int maxNumber)
     {
-        for (int counter = 0; counter <= 100; counter++)
+        for (int counter = 0; counter <= maxNumber; counter++)
         {
-            if (counter % 3 != 0 && counter % 5 != 0 && counter % 7 != 0 && counter % 11 != 0)
-            {
-                Console.WriteLine(counter);
-            }
-            else
-            {
-                Console.WriteLine((counter % 3 != 0 ? "" : "fizz") + (counter % 5 != 0 ? "" : "buzz") + (counter % 7 != 0 ? "" : "bang") + (counter % 11 != 0 ? "" : "boink"));
-            }
+            Console.WriteLine(
+              counter % 3 != 0 && counter % 5 != 0 && counter % 7 != 0 && counter % 11 != 0 && counter % 13 != 0
+              ?
+              counter
+              :
+                counter % 17 != 0
+                ?
+                (counter % 3 != 0 ? "" : "fizz") + (counter % 13 != 0 ? "" : "fezz") + (counter % 5 != 0 ? "" : "buzz") + (counter % 7 != 0 ? "" : "bang") + (counter % 11 != 0 ? "" : "bong")
+                : 
+                  ((counter % 11 != 0 ? "" : "bong") + (counter % 7 != 0 ? "" : "bang") + (counter % 5 != 0 ? "" : "buzz") + (counter % 13 != 0 ? "" : "fezz") + (counter % 3 != 0 ? "" : "fizz")));
         }
     }
 }
+
